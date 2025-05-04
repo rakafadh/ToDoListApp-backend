561804081308fe4566aa0cb4db1ae65af06cf5bf
rakafadh <maharaka@gmail.com>
2025-05-04 23:46:38 +0700

    backend

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: 'https://tutam9-maharakafadhilah.vercel.app' // Sesuaikan dengan domain frontend di Vercel
+  }));
 app.use(express.json());
 
 // Connect to MongoDB
